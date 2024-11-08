ca876377e1141fc39e6b559542e1608fa389c071
Alberto Leal <mail4alberto@gmail.com>
2024-11-08 15:26:49 -0500

    test

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from alberto!")
+import sentry_sdk
 
+sentry_sdk.init(
+    dsn="https://3a076cacc3dd1cdc233d62f06f484acd@o4507289866010624.ingest.us.sentry.io/4508264505606144",
+    traces_sample_rate=1.0,
+)
 
-if __name__ == "__main__":
-    main()
+sentry_sdk.profiler.start_profiler()
+
+print("Hello from alberto!")
+
+sentry_sdk.profiler.stop_profiler()
+
+print("bye")
